9e16854c878967278cb8d99d1e20230026d63828
Lawrence Mantin <lawrence.mantin@sprbrk.com>
2023-04-28 13:30:17 -0500

    Small tweeks

---
+++ OrdersDetails.cs
@@ -0,0 +1,12 @@
+﻿namespace Skeleton
+{
+    public class OrdersDetails
+    {
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public double Freight { get; set; }
+        public DateTime ShippedDate { get; set; }
+        public string ShipCity { get; set; }
+    }
+}
